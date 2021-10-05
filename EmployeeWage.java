a4e8c38bce4270a2b8f4fa90012cf3fc3a798e1e
shindeanant <shindeanant1997@gmail.com>
2021-10-05 23:17:46 +0530

    ADD: Calcualting monthly wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,16 @@
 package com.employeewageproblem;
 
+import java.util.Random;
+
 public class EmployeeWage {
 	int wagePerHours = 20;
 	int dailyHours = 8;
 	int dailyWage;
+	int employeeHour;
+	int daysPerMonth = 20;
+	int monthlyWage;
+	int partTimeHours = 4;
+	int partTimeWage;
 
 	public void employeeAttdence() {
 		int empCheck = (int) (Math.random() * 10) % 2;
@@ -20,10 +27,46 @@ public class EmployeeWage {
 		System.out.println("Employee Daily wage = " + dailyWage);
 	}
 
+	public void bySwitchCase() {
+		Random ran = new Random();
+		int empCheck = ran.nextInt(3);
+
+		switch (empCheck) {
+		case 0:
+			employeeHour = 8;
+			System.out.print("Present");
+			break;
+		case 1:
+			employeeHour = 4;
+			System.out.print("Part Time");
+			break;
+		case 2:
+			employeeHour = 0;
+			System.out.println("Absent");
+			break;
+		}
+	}
+
+	public void employeePartTimeWage() {
+
+		partTimeWage = wagePerHours * partTimeHours;
+		System.out.println("Employee Part Time Wage :" + partTimeWage);
+	}
+
+	public void monthlyWage() {
+
+		monthlyWage = dailyWage * daysPerMonth;
+		System.out.println("Monthly Wage Of Employee is =" + monthlyWage);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to the employee wage computaion problem");
 		EmployeeWage object = new EmployeeWage();
 		object.employeeAttdence();
+		object.bySwitchCase();
+		object.employeePartTimeWage();
+		object.dailyWage();
+		object.monthlyWage();
 
 	}
 
