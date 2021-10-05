4d7fe65d2ce3bdd025cbf1990fa32dfcaed248a2
shindeanant <shindeanant1997@gmail.com>
2021-10-05 22:36:23 +0530

    ADD: Welcome message

---
+++ EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.employeewageproblem;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to the employee wage computaion problem");
+
+	}
+
+}
